b45418274aeeff6090b46e83de79f78f75740a05
Viktor Kerkez <alefnula@gmail.com>
2020-02-27 12:14:44 +0000

    Remove the UI provided with arxiv.base. Remove arxiv from dependencies, and Flask too since it's already included in arxiv-base.

--- rss/factory.py
+++ rss/factory.py
@@ -2,7 +2,6 @@
 
 from flask import Flask
 
-from arxiv.base import Base
 from rss import routes
 
 
@@ -10,8 +9,6 @@ def create_web_app() -> Flask:
     """Initialize and configure the rss application."""
     app = Flask("rss")
     app.config.from_pyfile("config.py")
-
-    Base(app)  # Gives us access to the base UI templates and resources.
     app.register_blueprint(routes.blueprint)
 
     return app
