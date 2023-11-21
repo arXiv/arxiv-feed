cb4882b9440512861eb8ecca36e8c048e6636589
kyokukou <erin@arxiv.org>
2023-11-21 13:21:07 -0800

    arxiv update class has explicit properties

--- feed/tables.py
+++ feed/tables.py
@@ -1,34 +1,25 @@
-from sqlalchemy import MetaData, Integer, Column, Date, Enum, String, text, Index, Table, ForeignKey, DateTime, Text
+from sqlalchemy import MetaData, Integer, Column, Date, Enum, String, text, Index, ForeignKey, DateTime, Text
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
 
 metadata = MetaData()
 db= SQLAlchemy()
 
-
-###models for the arxiv_updates table
-arXiv_updates = Table(
-    'arXiv_updates', metadata,
-    Column('document_id', Integer(), index=True),
-    Column('version', Integer(), nullable=False, server_default=text("'1'")),
-    Column('date', Date, index=True),
-    Column('action', Enum('new', 'replace', 'absonly', 'cross', 'repcro')),
-    Column('archive', String(20), index=True),
-    Column('category', String(20), index=True),
-    Index('document_id', 'document_id', 'date', 'action', 'category', unique=True)
-)
-
 class ArXivUpdate(db.Model):
-    __table__ = arXiv_updates
-    # Define the composite primary key
-    __table_args__ = (
-        db.PrimaryKeyConstraint('document_id', 'date', 'action', 'category'),
+    __tablename__ = "arXiv_updates"
+    __table_args__ = (db.PrimaryKeyConstraint('document_id', 'date', 'action', 'category'),)
+
+    document_id = Column(
+        ForeignKey("arXiv_documents.document_id", ondelete="CASCADE", onupdate="CASCADE"),
+        nullable=False,
+        index=True,
+        server_default=text("'0'"),
     )
-    # Explicitly specify primary key columns
-    __mapper_args__ = {
-        'primary_key': [arXiv_updates.c.document_id, arXiv_updates.c.date, arXiv_updates.c.action, arXiv_updates.c.category]
-    }
-    # You can define indexes and additional attributes here
+    version=Column(Integer, nullable=False, server_default=text("'1'"))
+    date=Column( Date, index=True)
+    action=Column( Enum('new', 'replace', 'absonly', 'cross', 'repcro'))
+    archive=Column(String(20), index=True)
+    category=Column( String(20), index=True)
+
     def __repr__(self):
         return f"ArXivUpdate(document_id={self.document_id}, version={self.version}, action={self.action}, date={self.date}, category={self.category}, archive={self.archive})"
     
