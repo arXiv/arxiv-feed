d3f4a50273782f47f28b39a7444e2e70e10ce8da
kyokukou <erin@arxiv.org>
2023-11-22 10:44:54 -0800

    order by announce type, removed absonly

--- feed/database.py
+++ feed/database.py
@@ -1,12 +1,22 @@
 from typing import List, Tuple
-from sqlalchemy import and_, or_
+from sqlalchemy import and_, or_, case
 from feed.tables import ArXivUpdate, ArXivMetadata
 from datetime import datetime
 from sqlalchemy.orm import aliased
 
 def get_announce_papers(first_day: datetime, last_day: datetime, archives: List[str], categories: List[str])->List[Tuple[ArXivUpdate, ArXivMetadata]]:
-    result_limit = 15
+    result_limit = 50
     version_threshold = 4
+
+    action_order = case(
+    [
+        (ArXivUpdate.action == "new", 5),
+        (ArXivUpdate.action == "replace", 4),
+        (ArXivUpdate.action == "repcross", 3),
+        (ArXivUpdate.action == "cross", 2)
+    ],
+    else_=1  # Default order for other actions
+)
 
     # Create an alias for ArXivMetadata table to distinguish between the two
     metadata_alias = aliased(ArXivMetadata)
@@ -25,11 +35,11 @@ def get_announce_papers(first_day: datetime, last_day: datetime, archives: List[
                     ArXivUpdate.version <= version_threshold
                 )
             ),
-            ArXivUpdate.action != "abs_only",
+            ArXivUpdate.action != "absonly",
             ArXivUpdate.date >= first_day,
             ArXivUpdate.date <= last_day,  # Inclusive upper bound
             metadata_alias.is_current == 1  # dont display multiple version in a day TODO v4 and v5 in same day once version is fixed
         )
-    ).add_entity(metadata_alias).limit(result_limit).all()
+    ).order_by(action_order).add_entity(metadata_alias).limit(result_limit).all()
 
     return query_result
