5edd4ebcfbcfedc9ff7091c734e32ad1c4a63ae3
kyokukou <erin@arxiv.org>
2024-02-02 11:52:22 -0800

    logging for empty results

--- feed/database.py
+++ feed/database.py
@@ -1,8 +1,11 @@
 from typing import List, Tuple
+import logging 
 from sqlalchemy import and_, or_, case
 from feed.tables import ArXivUpdate, ArXivMetadata
 from datetime import date
 from sqlalchemy.orm import aliased
+
+logger = logging.getLogger(__name__)
 
 def get_announce_papers(first_day: date, last_day: date, archives: List[str], categories: List[str])->List[Tuple[ArXivUpdate, ArXivMetadata]]:
     result_limit = 2000
@@ -41,5 +44,8 @@ def get_announce_papers(first_day: date, last_day: date, archives: List[str], ca
             metadata_alias.is_current == 1  # dont display multiple version in a day TODO v4 and v5 in same day once version is fixed
         )
     ).order_by(action_order).add_entity(metadata_alias).limit(result_limit).all()
+
+    if len(query_result) <1:
+        logger.warning(f"No results for db query. first day: {first_day}, last day: {last_day}, archives: {archives}, categories: {categories}")
 
     return query_result # type: ignore
